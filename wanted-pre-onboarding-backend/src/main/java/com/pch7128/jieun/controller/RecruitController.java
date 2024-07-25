9ace0f69c37ceef673bb41cf6c91ba6d90e34b28
pch7128 <pch7128@naver.com>
2024-07-25 15:42:10 +0900

    first

---
+++ wanted-pre-onboarding-backend/src/main/java/com/pch7128/jieun/controller/RecruitController.java
@@ -0,0 +1,12 @@
+package com.pch7128.jieun.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/recruit")
+public class RecruitController {
+	
+	
+
+}
